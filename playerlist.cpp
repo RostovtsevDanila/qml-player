5c55684e09f73aa57ea591dd44b043ba0b813853
Valentin Veremchuk <Veremb@yandex.ru>
2019-11-27 01:31:26 +0300

    Добавлен показ файлов

--- playerlist.cpp
+++ playerlist.cpp
@@ -1,6 +1,71 @@
 #include "playerlist.h"
 
-PlayerList::PlayerList()
-{
+static QString PATH = QDir::homePath();
+//static QString PATHH = QStandardPaths::
 
+PlayerList::PlayerList(QObject *parent) : QAbstractListModel(parent)
+{
+    //    m_data.append("old");
+    //    m_data.append("another old");
+    qDebug() << PATH;
+    QDir way(PATH);
+    QStringList filters;
+    filters << "*.mp3";
+    m_data = way.entryInfoList(filters, QDir::AllEntries | QDir::NoDotAndDotDot);
 }
+
+QVariant PlayerList::data(const QModelIndex &index, int role) const
+{
+    if (!index.isValid()) {
+        return QVariant();
+    }
+
+    switch (role) {
+    case Name:
+        return m_data[index.row()].fileName();
+        //            return QVariant(index.row() < 2 ? "orange" : "skyblue");
+//    case Duration:
+//        return m_data;
+    default:
+        return QVariant();
+    }
+    //    switch (role) {
+    //    case ColorRole:
+    //        return QVariant(index.row() < 2 ? "orange" : "skyblue");
+    //    case TextRole:
+    //        return m_data.at(index.row());
+    //    default:
+    //        return QVariant();
+    //    }
+}
+
+int PlayerList::rowCount(const QModelIndex &parent) const
+{
+    if (parent.isValid()) {
+        return 0;
+    }
+    return m_data.size();
+}
+
+
+QHash<int, QByteArray> PlayerList::roleNames() const
+{
+    QHash<int, QByteArray> roles = QAbstractListModel::roleNames();
+    roles[Name] = "name";
+//            roles[Duration] = "duration";
+    //    QHash<int, QByteArray> roles = QAbstractListModel::roleNames();
+    //    roles[ColorRole] = "color";
+    //    roles[TextRole] = "text";
+    return roles;
+}
+
+//void PlayerList::add()
+//{
+//    beginInsertRows(QModelIndex(), m_data.size(), m_data.size());
+//    m_data.append("new");
+//    endInsertRows();
+
+//    m_data[0] = QString("Size: %1").arg(m_data.size());
+//    QModelIndex index = createIndex(0, 0, static_cast<void *>(0));
+//    emit dataChanged(index, index);
+//}
--- playerlist.h
+++ playerlist.h
@@ -2,11 +2,35 @@
 #define PLAYERLIST_H
 
 #include <QObject>
+#include <QAbstractListModel>
+#include <QStringList>
+#include <QDir>
+#include <QStandardPaths>
+#include <QDebug>
 
-class PlayerList
+class PlayerList : public QAbstractListModel
 {
+    Q_OBJECT
 public:
-    PlayerList();
+    enum Roles {
+        Name,
+//        Duration
+    };
+//    enum Roles {
+//        ColorRole = Qt::UserRole + 1,
+//        TextRole
+//    };
+
+    PlayerList(QObject *parent = nullptr);
+
+    int rowCount(const QModelIndex &parent) const;
+    QVariant data(const QModelIndex& index, int Role = Qt::DisplayRole) const;
+    QHash<int, QByteArray> roleNames() const;
+
+//    Q_INVOKABLE void add();
+
+private:
+    QList<QFileInfo> m_data;
 };
 
 #endif // PLAYERLIST_H
